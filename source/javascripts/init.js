ab64d4bbc1abd6c9952c162c5d380cef8c56ac63
szTheory <szTheory@users.noreply.github.com>
2021-06-19 14:14:11 -0400

    Remove Github stars widget. Formatting Sass code

--- source/javascripts/init.js
+++ source/javascripts/init.js
@@ -1,8 +1,6 @@
 import { runAsap } from "./run.js";
-import { setupStargazerCounters } from "./github.js";
 import { setupScroll } from "./navscroll.js";
 
 export const init = function () {
-  runAsap({ callbackFunction: setupStargazerCounters });
   runAsap({ callbackFunction: setupScroll });
 };
